8640a8ae6e926f9aeaae66a8a0759f55cc57ed3b
Toshiaki Maki <makingx@gmail.com>
2014-01-21 10:01:10 +0900

    java7 compatible

--- src/main/java/demo/welcome/WelcomeController.java
+++ src/main/java/demo/welcome/WelcomeController.java
@@ -4,14 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.time.LocalDateTime;
+//import java.time.LocalDateTime;
 
 @Controller
 public class WelcomeController {
     @RequestMapping("/")
     public String hello(Model model) {
         model.addAttribute("now",
-                LocalDateTime.now());
+                /*LocalDateTime.now()*/ new java.util.Date());
         return "welcome";
     }
 
